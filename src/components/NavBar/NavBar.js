d460ae979e6b0700a43102816c19f4ad8834553a
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-05-28 11:01:26 +0200

    created basic NavBar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,3 +1,9 @@
-const NavBar = () => {};
+import styled from "styled-components";
+
+const NavBarContainer = styled.section``;
+
+const NavBar = () => {
+  return <NavBarContainer></NavBarContainer>;
+};
 
 export default NavBar;
