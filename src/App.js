7f3028facfe78420610f0d7b4725542813bbd809
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-05-28 12:30:46 +0200

    added Description component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Description from "./components/Description/Description";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Description />
       <NavBar />
     </>
   );
---
+++ src/components/Description/Description.js
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+
+const DescriptionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  font-family: "Montserrat";
+
+  & h2 {
+    font-size: 24px;
+    font-weight: 500;
+    margin: 0;
+  }
+
+  & p {
+    font-size: 14px;
+    font-weight: 500;
+    margin: 0;
+  }
+`;
+
+const Description = () => {
+  return (
+    <DescriptionContainer>
+      <h2>Transactions history</h2>
+      <p>These are your monthly and daily actions. 📊</p>
+    </DescriptionContainer>
+  );
+};
+
+export default Description;
