0b6bf68ff859cda3068b7d30815291dde29561c0
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-05-28 13:05:58 +0200

    added SmallCardsList component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import Description from "./components/Description/Description";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
-import SmallCard from "./components/SmallCard/SmallCard";
+import SmallsCardsList from "./SmallsCardsList/SmallsCardsList";
 
 function App() {
   return (
     <>
       <Header />
       <Description />
-      <SmallCard day={"LAST MONTH"} daysNum={35} />
+      <SmallsCardsList />
       <NavBar />
     </>
   );
