4bb29e6debab00cedc2ddadfcf9248590acd521d
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2022-05-28 12:21:41 +0200

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
+      <Header />
       <NavBar />
     </>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+import NeoLogo from "../../svg/NeoLogo";
+
+const HeaderWrapper = styled.section`
+  display: flex;
+  align-items: center;
+  padding: 48px 0 30px 30px;
+
+  & h1 {
+    font-family: "Montserrat", sans-serif;
+    color: #4a35c5;
+    font-size: 30px;
+    font-weight: 900;
+    margin: 0;
+  }
+`;
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <NeoLogo />
+      <h1>BANKS</h1>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
